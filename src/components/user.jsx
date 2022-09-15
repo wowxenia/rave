fbee9f8c68ea7ac13f333d0a92ec8d79c31afdd6
Xenia <sendemailhereordie@gmail.com>
2022-09-15 12:57:05 +0300

    add best practices

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,94 +1,79 @@
 import React, { useState } from 'react';
-import API from '../api';
+import api from '../api';
 
 const Users = () => {
-  const [users, setUsers] = useState(API.users.fetchAll());
-  const getColor = 'm-2 badge bg-';
-
-  const nobodyRave = () => {
-    return (
-      <span className="badge bg-danger">
-        с тобой никто не хочет тусить, лох :(
-      </span>
-    );
-  };
+  const [users, setUsers] = useState(api.users.fetchAll());
 
   const handleDelete = (userId) => {
-    setUsers((prevState) => prevState.filter((user) => user._id !== userId));
+    setUsers(users.filter((user) => user._id !== userId));
   };
 
-  const renderPhrase = (number, titles) => {
-    let cases = [2, 0, 1, 1, 1, 2];
-    return titles[
-      number % 100 > 4 && number % 100 < 20
-        ? 2
-        : cases[number % 10 < 5 ? number % 10 : 5]
-    ];
+  const renderPhrase = (number) => {
+    const lastOne = Number(number.toString().slice(-1));
+    if (number > 4 && number < 15) return 'человек тусанет';
+    if ([2, 3, 4].indexOf(lastOne) >= 0) return 'человека тусанут';
+    if (lastOne === 1) return 'человек тусанет';
+    return 'человек тусанет';
   };
 
-  const renderContent = () => {
-    if (users.length > 0) {
-      return (
-        <div className="container">
-          <h1>
-            <span className="badge bg-primary">
-              {users.length}{' '}
-              {renderPhrase(users.length, ['человек', 'человека', 'человек'])}{' '}
-              тусанет с тобой сегодня
-            </span>
-          </h1>
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Имя</th>
-                <th scope="col">Качества</th>
-                <th scope="col">Профессия</th>
-                <th scope="col">Встретился, раз</th>
-                <th scope="col">Оценка</th>
-                <th scope="col"></th>
+  return (
+    <>
+      <h2>
+        <span
+          className={'badge ' + (users.length > 0 ? 'bg-primary' : 'bg-danger')}
+        >
+          {users.length > 0
+            ? `${
+                users.length + ' ' + renderPhrase(users.length)
+              } с тобой сегодня`
+            : 'Никто с тобой не тусанет'}
+        </span>
+      </h2>
+
+      {users.length > 0 && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>
+                  {user.qualities.map((item) => (
+                    <span
+                      className={'badge m-1 bg-' + item.color}
+                      key={item._id}
+                    >
+                      {item.name}
+                    </span>
+                  ))}
+                </td>
+                <td>{user.profession.name}</td>
+                <td>{user.completedMeetings}</td>
+                <td>{user.rate} /5</td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    className="btn btn-danger"
+                  >
+                    delete
+                  </button>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {users.map((user) => {
-                return (
-                  <tr key={user._id}>
-                    <td>{user.name}</td>
-                    <td>
-                      {user.qualities.map((qualitie) => {
-                        return (
-                          <span
-                            key={qualitie._id}
-                            className={getColor + qualitie.color}
-                          >
-                            {qualitie.name}
-                          </span>
-                        );
-                      })}
-                    </td>
-                    <td>{user.profession.name}</td>
-                    <td>{user.completedMeetings}</td>
-                    <td>{user.rate}</td>
-                    <td>
-                      <button
-                        onClick={() => handleDelete(user._id)}
-                        className="btn btn-danger"
-                      >
-                        delete
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
-      );
-    } else {
-      return <h1>{nobodyRave()}</h1>;
-    }
-  };
-
-  return <div className="container">{renderContent()}</div>;
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 };
 
 export default Users;
