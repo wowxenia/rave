8844fc84614f97b9006d7544b7166732c5b4c13c
Xenia <sendemailhereordie@gmail.com>
2022-11-12 00:42:32 +0200

    fix styles

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-// import UserPage from './components/userPage';
-// import Users from './components/users';
 import Users from './layouts/users';
 import Login from './layouts/login';
 import Main from './layouts/main';
@@ -12,7 +10,6 @@ const App = () => {
       <Switch>
         <Route path='/main' component={Main} />
         <Route path='/login' component={Login} />
-        {/* <Route path='/users' component={Users} /> */}
         <Route path='/users/:userId?' component={Users} />
       </Switch>
     </div>
--- src/app/components/quality.jsx
+++ src/app/components/quality.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 const Quality = ({ color, name }) => {
-  return <span className={'badge mr-1 bg-' + color}>{name}</span>;
+  return <span className={'badge me-1 bg-' + color}>{name}</span>;
 };
 Quality.propTypes = {
   color: PropTypes.string.isRequired,
